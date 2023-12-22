a5d071c702c7152291536a349e6848b01da7e189
Shubham Gupta <shubhamgupta@Shubhams-MacBook-Pro.local>
2023-12-22 16:36:55 +0800

    fixing error page

--- src/pages/error/error.tsx
+++ src/pages/error/error.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import './errorstyle.css'
+import { Button, Result } from 'antd';
+import { goToLogin } from '../../api/apiutil';
 
 const ErrorPage =()=>{
     return (
     <div className='main-view-container'>
-
+        <Result 
+        className='result-container'
+    status="404"
+    title="404"
+    subTitle="Sorry, the page you visited does not exist."
+    extra={<Button type="primary" onClick={(e)=>goToLogin()}>Go To Login</Button>}
+  />
     </div>
     );
 };
